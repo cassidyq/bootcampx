0eb74e51d519eef7c834039b75bbaab6c11345ec
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-02-03 23:05:40 +0000

    Added new directory for group by queries

--- 2_queries_joins/2_cohorts_total_assignment_duration.sql
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -2,4 +2,4 @@ SELECT sum(assignment_submissions.duration) as total_duration
 FROM assignment_submissions 
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = 'FEB12';
+WHERE cohorts.name = 'FEB12'q;
